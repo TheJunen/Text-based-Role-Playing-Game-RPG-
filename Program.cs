28405a8edf6d3731c55df644423e1776093e0a26
TheJunen <larshph00@gmail.com>
2024-12-09 19:08:54 +0100

    NB: Final Push. The project is complete.
    Added: counter and game completion counter, completed game logic, more game menu options like show stats, see added stats from statsupgrade and quit game, option to buy statsupgrade
    - Added buy statsupgrade where user can buy multiplie at the same time of chosen stat upgrade

--- Program.cs
+++ Program.cs
@@ -9,64 +9,125 @@ Min definisjon av spillet:
  * kan kjøpe sverd
  * Enkel form og grunnleggende funksjoner, bruk avanserte konsepter der det gir mening (se yt og internett)
 */
+using System.ComponentModel.Design;
 using System.Runtime.CompilerServices;
 using TextBasedRolePlayGame.Items;
 namespace TextBasedRolePlayGame
 {
-    class Program
+    class Program //lage mer utfordrende bosser
     {
+        public static int counter { get; private set; } = 0;
         static void Main(string[] args)
         {
-            MainCharacter mainCharacter = new MainCharacter(100, 1, 10, 10);
-            Boss bossLvl1 = new Boss("Boss lvl 1", 200, 1, 2, 1, "Lvl 1 boss");
+            //starts Gametime Counter in a new thread
+            Thread counterThread = new Thread(CounterGametime);
+            counterThread.IsBackground = true; //makes it a background thread so counter will stop when main program stops
+            counterThread.Start();
+
+            MainCharacter mainCharacter = new MainCharacter(100, 1, 10, 1);
+            Boss bossLvl1 = new Boss("Boss lvl 1", 120, 1, 2, 1, "Lvl 1 boss");
             Boss bossLvl2 = new Boss("Boss lvl 2", 300, 2, 4, 2, "Lvl 2 boss");
-            Boss bossLvl3 = new Boss("Boss lvl 3", 400, 3, 6, 3, "Lvl 3 boss");
+            Boss bossLvl3 = new Boss("Boss lvl 3", 450, 3, 6, 3, "Lvl 3 boss");
 
             Shop shop = new Shop();
             GameLogic gameLogic = new GameLogic();
-
-            List<Boss> defeatedBossesList = new List<Boss>();
 
             bool runningGame = true;
 
             while (runningGame)
             {
                 Console.WriteLine("Hello and welcome to Text-based Role Playing Game. This consists of 3 bosses ranging from lvl 1-3. " +
-                    "Each time you defeat a boss for the first time it will add to your story progress bar. You can can continue to fight previous defeated bosses to gain rewards.");
-                Console.WriteLine("You can buy items from the shop and the items will be added to your inventory. while you are not fighting a boss, you can visit the shop and customise your characters equioment");
+                    "Each time you defeat a boss for the first time it will add to your story progress bar. You can continue to fight previous defeated bosses to gain rewards.");
+                Console.WriteLine("You can buy items from the shop and the items will be added to your inventory. While you are not fighting a boss, you can visit the shop and customise your characters equioment");
                 Console.WriteLine();
                 Console.WriteLine("Let's get started with your characters name:");
                 mainCharacter.Name = Console.ReadLine();
                 Console.WriteLine($"Nice to meet you, {mainCharacter.Name}");
 
+                Console.WriteLine($"{mainCharacter.Name} starting stats:");
+
+                mainCharacter.PrintOutStats();
+
+
                 bool runningMenu = true;
+                bool gameCompletionMessageShown = false;
 
                 while (runningMenu)
                 {
-                    Console.WriteLine("Welcome to the menus. Write 1 to visit the shop, 2 to see inventory and choose to customise your characters equipment or 3 to fight bosses.");
+                    if (gameLogic.StoryPercentBar == 100 && gameCompletionMessageShown == false)
+                    {
+                        Console.WriteLine("Congratulations! You have completed the game!");
+                        CounterGameTimeGameCompletion();
+                        Console.WriteLine();
+                        Console.WriteLine("Write 'finish' to exit the game or 'continue' to keep playing the game.");
 
-                    switch (Console.ReadLine())
+                        gameCompletionMessageShown = true;
+                        bool validChoice = false;
+                        string choice = "";
+                        while (!validChoice)
+                        {
+                            choice = Console.ReadLine().ToLower();
+
+                            if (choice == "finish")
+                            {
+                                validChoice = true;
+                                runningGame = false;
+                                Console.WriteLine("You have chosen to end the game. Quitting game now..");
+                                break;
+                            }
+                            else if (choice == "continue")
+                            {
+                                validChoice = true;
+                                Console.WriteLine("You have chosen to continue to play. Going back to the menu now..");
+                            }
+                        }
+                        if (choice == "finish")
+                        {
+                            break;
+                        }
+                    }
+
+                    double storyPercentBar = gameLogic.StoryPercentBar;
+                    double roundedValueStoryPercentBar = Math.Round(storyPercentBar, 2);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Story percent bar: {roundedValueStoryPercentBar} %");
+                    Console.WriteLine($"{mainCharacter.Name} currently coins: {mainCharacter.Coins}");
+
+                    Console.WriteLine($"Welcome to the menus. Write 1 to visit the shop, 2 to see inventory, 3 to choose to customise your characters equipment," +
+                        $" 4 to see {mainCharacter.Name} stats, 5 to see added stats from statsupgrade, 6 to fight bosses or 7 to quit the game.");
+
+                    switch (Console.ReadLine()) //menu with 7 options
                     {
                         case "1":
-                            shop.VisitShop(mainCharacter);
+                            shop.VisitShop(mainCharacter); //visits the shop
                             break;
                         case "2":
+                            mainCharacter.PrintOutInventory(); //prints out the inventory
+                            break;
+                        case "3": //customise main character's equipments
                             Console.WriteLine("Write 1 to equip items from inventory or 2 to remove equipped items");
 
                             switch (Console.ReadLine())
                             {
                                 case "1":
-                                    mainCharacter.EquipItemsFromInventory(mainCharacter);
+                                    mainCharacter.EquipItemsFromInventory();
                                     break;
                                 case "2":
-                                    mainCharacter.RemoveEquippedItems(mainCharacter);
+                                    mainCharacter.RemoveEquippedItems();
                                     break;
                                 default:
                                     Console.WriteLine("Error. Write 1 or 2.");
                                     break;
                             }
                             break;
-                        case "3":
+                        case "4": //prints out main character's stats
+                            mainCharacter.PrintOutStats();
+                            break;
+                        case "5": //checks and prints out main character's upgraded stats through shop's stats upgrade
+                            shop.CheckerStatsUpgrade();
+                            break;
+                        case "6": //boss fight menu for fighting previous or new bosses
                             bool runningBossMenu = true;
 
                             while (runningBossMenu)
@@ -76,11 +137,11 @@ namespace TextBasedRolePlayGame
                                 switch (Console.ReadLine())
                                 {
                                     case "1":
-                                        if (defeatedBossesList.Count > 0)
+                                        if (gameLogic.defeatedBossesList.Count > 0)
                                         {
                                             Console.WriteLine("You have the possibility to fight these previously defeated bosses:");
 
-                                            foreach (var boss in defeatedBossesList)
+                                            foreach (var boss in gameLogic.defeatedBossesList)
                                             {
                                                 Console.WriteLine(boss.Name);
                                             }
@@ -90,8 +151,24 @@ namespace TextBasedRolePlayGame
                                             {
                                                 case "1":
                                                     Console.WriteLine("You have chosen to fight. Write 1 to fight nr 1 from list, 2 for nr2 etc.");
-                                                    int choice = int.Parse(Console.ReadLine()) - 1;
-                                                    gameLogic.BossFightSimulator(mainCharacter, defeatedBossesList[choice]);
+
+                                                    bool correctChoiceInput = false;
+                                                    int choiceItem = 0;
+
+                                                    while (!correctChoiceInput)
+                                                    {
+                                                        if (int.TryParse(Console.ReadLine(), out choiceItem) && choiceItem <= gameLogic.defeatedBossesList.Count)
+                                                        {
+                                                            choiceItem -= 1;
+                                                            correctChoiceInput = true;
+                                                        }
+                                                        else
+                                                        {
+                                                            Console.WriteLine("Error. Write a valid number that are in the list range.");
+                                                        }
+                                                    }
+
+                                                    gameLogic.BossFightSimulator(mainCharacter, gameLogic.defeatedBossesList[choiceItem]);
                                                     break;
                                                 default:
                                                     Console.WriteLine("Returning back to boss menu");
@@ -106,26 +183,54 @@ namespace TextBasedRolePlayGame
                                     case "2":
                                         if (bossLvl1.Defeated == false)
                                         {
-                                            Console.WriteLine($"You have not defeated {bossLvl1.Name}. Fight starts..");
+                                            Console.WriteLine($"You have not defeated {bossLvl1.Name}. Write enter to continue.");
+                                            Console.ReadLine();
                                             gameLogic.BossFightSimulator(mainCharacter, bossLvl1);
                                         }
                                         else if (bossLvl2.Defeated == false)
                                         {
-                                            Console.WriteLine($"You have not defeated {bossLvl2.Name}. Fight starts..");
+                                            Console.WriteLine($"You have not defeated {bossLvl2.Name}. Write enter to continue.");
+                                            Console.ReadLine();
                                             gameLogic.BossFightSimulator(mainCharacter, bossLvl2);
                                         }
                                         else
                                         {
-                                            Console.WriteLine($"You have not defeated {bossLvl3.Name}. Fight starts..");
+                                            Console.WriteLine($"You have not defeated {bossLvl3.Name}. Write enter to continue.");
+                                            Console.ReadLine();
                                             gameLogic.BossFightSimulator(mainCharacter, bossLvl3);
                                         }
                                         break;
                                     case "3":
+                                        Console.WriteLine("Going back to the menu");
                                         runningBossMenu = false;
                                         break;
                                     default:
                                         Console.WriteLine("Error. Write 1,2 or 3.");
                                         break;
+                                }
+                            }
+                            break;
+                        case "7": //quit game option and verification check that ask you one more time if you want to quit 
+                            Console.WriteLine("Are you sure you want to quit the game? You will lose all progress. Write 'quit' to quit the game or 'continue' to continue the game.");
+
+                            bool validChoice = false;
+
+                            while (!validChoice)
+                            {
+                                string choiceItem = Console.ReadLine().ToLower();
+                                
+                                if (choiceItem == "quit")
+                                {
+                                    runningMenu = false;
+                                    runningGame = false;
+                                    validChoice = true;
+                                    Console.WriteLine("You have chosen to quit the game. Quitting game now.");
+                                    break;
+                                }
+                                else if (choiceItem == "continue")
+                                {
+                                    validChoice = true;
+                                    Console.WriteLine("You have chosen to continue the game. Going back to the menu.");
                                 }
                             }
                             break;
@@ -135,6 +240,29 @@ namespace TextBasedRolePlayGame
                     }
                 }
             }
+        }
+
+        static void CounterGametime() //counts the game time each minute
+        {
+            int minuteCounter = 0;
+
+            while (true)
+            {
+                if (counter % 60 == 0 && counter > 0)
+                {
+                    minuteCounter++;
+                    Console.WriteLine($"Counter: {minuteCounter} minutes played");
+                }
+                Thread.Sleep(1000);
+                counter++;
+            }
+        }
+
+        static void CounterGameTimeGameCompletion()
+        {
+            int minuteCounter = counter / 60;
+
+            Console.WriteLine($"Counter for completed game: {minuteCounter} minutes played");
         }
     }
 }
--- Shop.cs
+++ Shop.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using TextBasedRolePlayGame.Items;
 
 namespace TextBasedRolePlayGame
 {
     internal class Shop
     {
+        public int NumberAttackStatUpgrade { get; private set; }
+        public int NumberDefenseStatUpgrade { get; private set; }
+        public int NumberHPStatUpgrade { get; private set; }
+
         public List<Item> shopItems { get; private set; } = new List<Item>
         {
                 new Sword("Sword lvl 1", 10, 20, "Level 1 sword"),
@@ -23,28 +28,164 @@ namespace TextBasedRolePlayGame
             while (!doneVisitShop)
             {
                 Console.WriteLine("Welcome to the shop.");
-                Console.WriteLine("Items in the shop:");
-                foreach (Item item in shopItems)
+
+                Console.WriteLine("Write 1 to shop items in shop, 2 to shop for stats upgrade or 3 to exit shop");
+
+                var choiceMenu = Console.ReadLine();
+
+                if (choiceMenu == "1") //main menu for shop items and stats upgrade
                 {
-                    item.WriteOutInfo();
+                    Console.WriteLine("Items in the shop:");
+                    foreach (Item item in shopItems)
+                    {
+                        item.WriteOutInfo();
+                    }
+
+                    Console.WriteLine($"{mainCharacter.Name}'s coins: {mainCharacter.Coins}");
+
+                    Console.WriteLine("Write 1 to buy nr 1 from list, 2 for nr 2 etc");
+
+                    bool correctChoiceInput = false;
+                    int choiceItem = 0;
+
+                    while (!correctChoiceInput)
+                    {
+                        if (int.TryParse(Console.ReadLine(), out choiceItem))
+                        {
+                            choiceItem -= 1;
+                            correctChoiceInput = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error. Write a valid number");
+                        }
+
+                    }
+
+
+                    if (shopItems[choiceItem].Price <= mainCharacter.Coins)
+                    {
+                        mainCharacter.Coins -= shopItems[choiceItem].Price;
+                        mainCharacter.AddItemToInventory(shopItems[choiceItem]);
+                        Console.WriteLine($"The buy of {shopItems[choiceItem].Name} was successful and {shopItems[choiceItem].Price} was deducted." +
+                            $" Item {shopItems[choiceItem].Name} was added to {mainCharacter.Name}'s inventory. The balance is {mainCharacter.Coins}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{mainCharacter.Name} don't have enough coins. Balance is {mainCharacter.Coins} coins. {shopItems[choiceItem].Name} cost {shopItems[choiceItem].Price} coins.");
+                    }
                 }
-
-                Console.WriteLine($"{mainCharacter.Name}'s coins: {mainCharacter.Coins}");
-
-                Console.WriteLine("Write 1 to buy nr 1 from list, 2 for nr 2 etc");
-
-                int choice = int.Parse(Console.ReadLine()) - 1;
-
-                if (shopItems[choice].Price <= mainCharacter.Coins)
+                else if (choiceMenu == "2") //stats upgrade
                 {
-                    mainCharacter.Coins -= shopItems[choice].Price;
-                    mainCharacter.AddItemToInventory(shopItems[choice]);
-                    Console.WriteLine($"The buy of {shopItems[choice].Name} was successful and {shopItems[choice].Price} was deducted." +
-                        $" Item {shopItems[choice].Name} was added to {mainCharacter.Name}'s inventory. The balance is {mainCharacter.Coins}.");
+                    bool runningStatsUpgradeMenu = true;
+                    while (runningStatsUpgradeMenu)
+                    {
+                        Console.WriteLine("The attack, defense and HP upgrades each adds +1 stat to their respective categories. Each upgrade costs 2 coins.");
+                        Console.WriteLine("Write 1 to buy Attack upgrade, 2 to buy Defense upgrade, 3 to buy HP upgrade or 4 to return to shop menu");
+
+                        var choiceStatsUpgradeMenu = Console.ReadLine();
+
+                        if (choiceStatsUpgradeMenu == "1")
+                        {
+                            Console.WriteLine("Write how many you want to buy of attack upgrade");
+                            int amountAttackUpgrade = 0;
+                            bool validAmountAnswer = false;
+
+                            while (!validAmountAnswer)
+                            {
+                                string choice = Console.ReadLine();
+
+                                if (int.TryParse(choice, out amountAttackUpgrade) && mainCharacter.Coins >= amountAttackUpgrade * 2)
+                                {
+                                    validAmountAnswer = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error. Write a number and within your coins budget.");
+                                }
+                            }
+                            if (mainCharacter.Coins >= 2 * amountAttackUpgrade)
+                            {
+                                mainCharacter.Coins -= 2 * amountAttackUpgrade;
+                                AttackUpgrade(mainCharacter, amountAttackUpgrade);
+                                Console.WriteLine($"Attack Upgrade purchased {amountAttackUpgrade} times");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{mainCharacter.Name} don't have enough coins");
+                            }
+                        }
+                        else if (choiceStatsUpgradeMenu == "2")
+                        {
+                            Console.WriteLine("Write how many you want to buy of defense upgrade");
+                            int amountDefenseUpgrade = 0;
+                            bool validAmountAnswer = false;
+
+                            while (!validAmountAnswer)
+                            {
+                                string choice = Console.ReadLine();
+
+                                if (int.TryParse(choice, out amountDefenseUpgrade) && mainCharacter.Coins >= amountDefenseUpgrade * 2)
+                                {
+                                    validAmountAnswer = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error. Write a number and within your coins budget.");
+                                }
+                            }
+                            if (mainCharacter.Coins >= 2 * amountDefenseUpgrade)
+                            {
+                                mainCharacter.Coins -= 2 * amountDefenseUpgrade;
+                                DefenseUpgrade(mainCharacter, amountDefenseUpgrade);
+                                Console.WriteLine($"Defense Upgrade purchased {amountDefenseUpgrade} times");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{mainCharacter.Name} don't have enough coins");
+                            }
+                        }
+                        else if (choiceStatsUpgradeMenu == "3")
+                        {
+                            Console.WriteLine("Write how many you want to buy of HP upgrade");
+                            int amountHPUpgrade = 0;
+                            bool validAmountAnswer = false;
+
+                            while (!validAmountAnswer)
+                            {
+                                string choice = Console.ReadLine();
+
+                                if (int.TryParse(choice, out amountHPUpgrade) && mainCharacter.Coins >= amountHPUpgrade * 2)
+                                {
+                                    validAmountAnswer = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error. Write a number and within your coins budget.");
+                                }
+                            }
+                            if (mainCharacter.Coins >= 2 * amountHPUpgrade)
+                            {
+                                mainCharacter.Coins -= 2 * amountHPUpgrade;
+                                HPUpgrade(mainCharacter, amountHPUpgrade);
+                                Console.WriteLine($"HP Upgrade purchased {amountHPUpgrade} times");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{mainCharacter.Name} don't have enough coins");
+                            }
+                        }
+                        else if (choiceStatsUpgradeMenu == "4")
+                        {
+                            runningStatsUpgradeMenu = false;
+                            Console.WriteLine("Returning back to shop menu");
+                        }
+                    }
                 }
-                else
+                else if (choiceMenu == "3")
                 {
-                    Console.WriteLine($"{mainCharacter.Name} don't have enough coins. Balance is {mainCharacter.Coins} coins. {shopItems[choice].Name} cost {shopItems[choice].Price} coins.");
+                    doneVisitShop = true;
+                    break;
                 }
 
                 Console.WriteLine("Write 1 to continue shopping, 2 to quit");
@@ -55,6 +196,34 @@ namespace TextBasedRolePlayGame
                     Console.WriteLine("Exited shop");
                 }
             }
+        }
+
+        private void AttackUpgrade(MainCharacter mainCharacter, int amount) //adds specifed amount to ättack stats
+        {
+            mainCharacter.AttackPower += amount;
+            NumberAttackStatUpgrade += amount;
+
+            Console.WriteLine($"+{amount} attack was added to {mainCharacter.Name}");
+        }
+        private void DefenseUpgrade(MainCharacter mainCharacter, int amount) //adds specifed amount to defense stats
+        {
+            mainCharacter.DefensePower += amount;
+            NumberDefenseStatUpgrade += amount;
+
+            Console.WriteLine($"+{amount} defense was added to {mainCharacter.Name}");
+        }
+
+        private void HPUpgrade(MainCharacter mainCharacter, int amount) //adds specifed amount to HP stats
+        {
+            mainCharacter.OriginalHP += amount;
+            NumberHPStatUpgrade += amount;
+
+            Console.WriteLine($"+{amount} HP was added to {mainCharacter.Name}");
+        }
+
+        internal void CheckerStatsUpgrade() //Prints out added stats on all upgradeable categories
+        {
+            Console.WriteLine($"You have added +attack stat: {NumberAttackStatUpgrade}, +defense stat: {NumberDefenseStatUpgrade} and +HP stat: {NumberHPStatUpgrade}");
         }
     }
 }
